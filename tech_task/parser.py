eaf23bce1dab5b2684266d4245f313707f671cc1
DmitryPolyan <dmitrystomper@gmail.com>
2021-09-28 01:24:21 +0300

    parser is done

---
+++ tech_task/parser.py
@@ -0,0 +1,52 @@
+import gzip
+from typing import List
+
+
+def parse_file(path: str) -> List[dict]:
+    """
+    :param path: Путь до исходного текстового файла
+    :return: список словарей
+    """
+    if path.endswith('.gz'):
+        with gzip.open(path, 'rt', encoding='utf-8') as file:
+            return dicts_maker(file)
+    else:
+        with open(path, 'r') as file:
+            return dicts_maker(file)
+
+
+def dicts_maker(file) -> List[dict]:
+    """
+    Создание словарей из документов из файла
+    :param file: файл с документами
+    :return: Список словарей
+    """
+    temp_dict = dict()
+    dicts = []
+    for string in file:
+        if string[0] == '#':
+            continue
+        if string == '\n':
+            if len(temp_dict):
+                dicts.append(temp_dict)
+                temp_dict = dict()
+            continue
+        if string[0] != ' ':
+            key, value = string.split(':', maxsplit=1)
+            if key in temp_dict:
+                temp_dict[key] = temp_dict[key] + '\n' + value.lstrip()[:-1]
+            else:
+                temp_dict[key] = value.lstrip()[:-1]
+        else:
+            temp_dict[key] = temp_dict[key] + '\n' + string.lstrip()[:-1]
+    if len(temp_dict):
+        dicts.append(temp_dict)
+    return dicts
+
+
+def load_data(path: str):
+    parsed_data = parse_file(path)
+
+    for document in parsed_data:
+        ...
+        # load document to database (or do something else)
--- tests/test_tech_task.py
+++ tests/test_tech_task.py
@@ -1,5 +1,40 @@
-from tech_task import __version__
+from tech_task.parser import dicts_maker
+from tech_task.parser import parse_file
+
+example_result = {'as-block': 'AS30720 - AS30979',
+'type': 'REGULAR',
+'descr': 'RIPE NCC ASN block',
+'remarks': 'These AS Numbers are further assigned to network\n' \
+           'operators in the RIPE NCC service region. AS\n' \
+           'assignment policy is documented in:\n' \
+           '<http://www.ripe.net/ripe/docs/asn-assignment.html>\n' \
+           'RIPE NCC members can request AS Numbers using the\n' \
+           'form located at:\n' \
+           '<http://lirportal.ripe.net/lirportal/liruser/resource_request/draw.html?name=as-number>\n' \
+           'data has been transferred from RIPE Whois Database 20050221',
+'org': 'ORG-AFNC1-AFRINIC',
+'admin-c': 'TEAM-AFRINIC',
+'tech-c': 'TEAM-AFRINIC',
+'mnt-by': 'AFRINIC-HM-MNT',
+'mnt-lower': 'AFRINIC-HM-MNT',
+'changed': '***@ripe.net 20031001\n' \
+           '***@ripe.net 20040421\n' \
+           '***@ripe.net 20050202\n' \
+           '***@afrinic.net 20050205',
+'source': 'AFRINIC'}
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_dicts_maker():
+    path = 'tests/example.txt'
+    with open(path, 'r') as file:
+        assert dicts_maker(file)[0] == example_result
+
+
+def test_parse_txt():
+    path = 'tests/example.txt'
+    assert parse_file(path)[0] == example_result
+
+
+def test_parse_gzip():
+    path = 'tests/file.txt.gz'
+    assert parse_file(path)[0] == example_result
